4841466c5cfc7cf2a315622a9dee2b5769303078
Unknown <dadfar.momeni@gmail.com>
2018-01-28 23:02:40 +0330

    crashing!!!

--- plane.cpp
+++ plane.cpp
@@ -13,7 +13,8 @@ using namespace std;
 void initialization (int row[],int column[], int &basecount);
 void show(SDL_Surface* screen, int cloud_speed, int basecount);
 void event_handel (SDL_Surface* screen, int basecount);
-void base_handel (double timer, bool &time_flag, int basecount);
+void base_handel (double &timer, int basecount);
+void optimize (SDL_Surface* screen);
 struct base_struct
 {
 	char type;
@@ -49,8 +50,8 @@ int main (int argc, char * args[])
 	int row[3]={100,340,580};
 	SDL_Surface* screen= NULL;
 	screen = SDL_SetVideoMode (1280, 720, 32, SDL_SWSURFACE);
+	SDL_WM_SetCaption ("Plane Wars (D&H)", NULL);
 	SDL_Surface* background = NULL;
-	SDL_Surface* myplane =NULL;
 	SDL_Surface* foeplane =NULL;
 	SDL_Surface* boom =NULL;
 	SDL_Surface* planeshadow=NULL;
@@ -63,24 +64,16 @@ int main (int argc, char * args[])
 	cloud[2].y=450;
 	int basecount =0;
 	double timer=time(0);
-	cout<<"kir";
-	bool time_flag=true;
 	int cloud_speed=1;
 	initialization (row,column, basecount);
 	while (true)
 	{
-		base_handel (timer, time_flag, basecount);
 		SDL_BlitSurface(background, NULL, screen, NULL );
 		event_handel (screen, basecount);
-		show (screen,cloud_speed, basecount);
-		if (!time_flag)
-		{
-			timer=time(0);
-			time_flag=true;
-		}
-		
+		//show (screen,cloud_speed, basecount);
+		base_handel (timer, basecount);
 		SDL_Flip(screen);
-		SDL_Delay (1);
+		SDL_Delay (2);
 	}
 	return 0;
 }
@@ -139,7 +132,8 @@ void initialization (int row[],int column[], int &basecount)
 				base[counter].w=85;
                 base[counter].h=89;
                 base[counter].select=false;
-                base[counter].plane_count=0;
+                base[counter].plane_count=5;
+                base[counter].production_speed=1;
 			}
 			else
 			{
@@ -149,7 +143,8 @@ void initialization (int row[],int column[], int &basecount)
 				base[counter].w=76;
                 base[counter].h=85;
                 base[counter].select=false;
-                base[counter].plane_count=0;
+                base[counter].plane_count=5;
+                base[counter].production_speed=1;
 			}
 		}
 		counter++;
@@ -167,6 +162,7 @@ void show(SDL_Surface* screen, int cloud_speed, int basecount)
 	SDL_Surface* mybase_select=NULL;
 	SDL_Surface* foebase_select=NULL;
 	SDL_Surface* emptybase_select=NULL;
+	SDL_Surface* mybase_plane_count[20];
 	mybase= IMG_Load ("game_images/mybase.png");
 	foebase= IMG_Load ("game_images/foebase.png");
 	emptybase= IMG_Load ("game_images/emptybase.png");
@@ -176,12 +172,34 @@ void show(SDL_Surface* screen, int cloud_speed, int basecount)
 	baseshadow=IMG_Load ("game_images/baseshadow.png");
 	Cloud=IMG_Load ("game_images/cloud.png");
 	cloudshadow=IMG_Load("game_images/cloudshadow.png");
+	mybase_plane_count[0]= IMG_Load ("game_images/numbers/1.png");
+	mybase_plane_count[1]= IMG_Load ("game_images/numbers/2.png");
+	mybase_plane_count[2]= IMG_Load ("game_images/numbers/3.png");
+	mybase_plane_count[3]= IMG_Load ("game_images/numbers/4.png");
+	mybase_plane_count[4]= IMG_Load ("game_images/numbers/5.png");
+	mybase_plane_count[5]= IMG_Load ("game_images/numbers/6.png");
+	mybase_plane_count[6]= IMG_Load ("game_images/numbers/7.png");
+	mybase_plane_count[7]= IMG_Load ("game_images/numbers/8.png");
+	mybase_plane_count[8]= IMG_Load ("game_images/numbers/9.png");
+	mybase_plane_count[9]= IMG_Load ("game_images/numbers/10.png");
+	mybase_plane_count[10]= IMG_Load ("game_images/numbers/11.png");
+	mybase_plane_count[11]= IMG_Load ("game_images/numbers/12.png");
+	mybase_plane_count[12]= IMG_Load ("game_images/numbers/13.png");
+	mybase_plane_count[13]= IMG_Load ("game_images/numbers/14.png");
+	mybase_plane_count[14]= IMG_Load ("game_images/numbers/15.png");
+	mybase_plane_count[15]= IMG_Load ("game_images/numbers/16.png");
+	mybase_plane_count[16]= IMG_Load ("game_images/numbers/17.png");
+	mybase_plane_count[17]= IMG_Load ("game_images/numbers/18.png");
+	mybase_plane_count[18]= IMG_Load ("game_images/numbers/19.png");
+	mybase_plane_count[19]= IMG_Load ("game_images/numbers/20.png");
+	SDL_Rect mybase_plane_count_cords;
 	SDL_Rect mybase_cords;
 	SDL_Rect foebase_cords;
 	SDL_Rect emptybase_cords;
 	SDL_Rect baseshadow_cords;
 	SDL_Rect cloud_cords;
 	SDL_Rect cloudshadow_cords;
+
 	for (int i=0;i<basecount;i++)
 	{
 		if (base[i].type=='m')
@@ -193,12 +211,16 @@ void show(SDL_Surface* screen, int cloud_speed, int basecount)
 			SDL_BlitSurface (baseshadow, NULL, screen, &baseshadow_cords);
 			if (base[i].select)
 			{
-			SDL_BlitSurface( mybase_select, NULL, screen, &mybase_cords);	
+			SDL_BlitSurface( mybase_select, NULL, screen, &mybase_cords);
 			}
-			else 
+			else
 			{
 				SDL_BlitSurface( mybase, NULL, screen, &mybase_cords);
-			}	
+			}
+			mybase_plane_count_cords.x=base[i].x+22;
+			mybase_plane_count_cords.y=base[i].y-25;
+			SDL_BlitSurface (mybase_plane_count[base[i].plane_count-1], NULL, screen, &mybase_plane_count_cords);
+
 		}
 		if (base[i].type=='f')
 		{
@@ -209,12 +231,12 @@ void show(SDL_Surface* screen, int cloud_speed, int basecount)
 			SDL_BlitSurface (baseshadow, NULL, screen, &baseshadow_cords);
 			if (base[i].select)
 			{
-			SDL_BlitSurface( foebase_select, NULL, screen, &foebase_cords);	
+			SDL_BlitSurface( foebase_select, NULL, screen, &foebase_cords);
 			}
-			else 
+			else
 			{
 				SDL_BlitSurface( foebase, NULL, screen, &foebase_cords);
-			}	
+			}
 		}
 		if (base[i].type=='e')
 		{
@@ -227,10 +249,10 @@ void show(SDL_Surface* screen, int cloud_speed, int basecount)
 			{
 				SDL_BlitSurface( emptybase_select, NULL, screen, &emptybase_cords);
 			}
-			else 
+			else
 			{
 				SDL_BlitSurface( emptybase, NULL, screen, &emptybase_cords);
-			}	
+			}
 		}
 	}
 	for (int i=0;i<3;i++)
@@ -290,33 +312,37 @@ void event_handel (SDL_Surface* screen, int basecount)
                 	if (base[i].type!='m')
                 	{
                     	base[i].select=true;
-                    }	
+                    }
                 }
                 else if (base[i].type!='m')
-            	{	
+            	{
             		base[i].select=false;
             	}
             }
             else if (base[i].type!='m')
-            {	
+            {
             	base[i].select=false;
             }
         }
 	}
 }
-void base_handel (double timer, bool &time_flag, int basecount)
+void base_handel (double &timer, int basecount)
 {
-	bool current_time=time(0); 
-	if (current_time - timer>=2000)
+	double current_time=time(0);
+	if (current_time - timer>=2)
 	{
 		for (int i=0;i<basecount;i++)
-		{		
-			if (base[i].type!='e' && base[i].plane_count<=20)
-			{	
+		{
+			if (base[i].type!='e' && base[i].plane_count<20)
+			{
 				base[i].plane_count+=base[i].production_speed;
-				cout<<i<<" :	"<<base[i].plane_count<<endl;
 			}
 		}
-		time_flag=false;
+		timer=time(0);
 	}
+}
+void optimize (SDL_Surface* screen)
+{
+
+
 }
